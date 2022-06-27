515d2581e5d7ab6eac561fe10ac420f0b1b5d68b
Rıdvan Akca <akca.ridvann@gmail.com>
2022-06-27 17:03:03 +0300

    file structure is done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import "./App.css";
+import "./App.scss";
+
+import { About, Header, Footer, Skills, Testimonial, Work } from "./container";
+import { Navbar } from "./components";
 
 function App() {
-  return <>App</>;
+  return (
+    <div className='app'>
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
