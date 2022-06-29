d35d17837a88393038a656bd6956562a1b354b5d
Rıdvan Akca <akca.ridvann@gmail.com>
2022-06-29 12:49:50 +0300

    abouts data are fetched from sanity

--- src/container/About/index.jsx
+++ src/container/About/index.jsx
@@ -2,15 +2,17 @@ import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import { images } from "../../constants";
 import "./About.scss";
-
-const abouts = [
-  { title: "Web Development", description: "I am a good web developer", imgUrl: images.about01 },
-  { title: "Frontend Development", description: "I am a good frontend developer", imgUrl: images.about02 },
-  { title: "Backend Development", description: "I am a good backend developer", imgUrl: images.about03 },
-  { title: "MERN Stack", description: "I am a good mern stack developer", imgUrl: images.about04 },
-];
+import { urlFor, client } from "../../client";
 
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => setAbouts(data));
+  }, []);
+
   return (
     <>
       <h2 className='head-text'>
@@ -21,7 +23,7 @@ const About = () => {
       <div className='app__profiles'>
         {abouts.map((about, index) => (
           <motion.div whileInView={{ opacity: 1 }} whileHover={{ scale: 1.1 }} transition={{ duration: 0.5, type: "tween" }} className='app__profile-item' key={about.title + index}>
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className='bold-text' style={{ marginTop: 20 }}>
               {about.title}
             </h2>
